3cd900558158a76e8143d8063676207874ab5294
Ayush Tiwari <codewithayush77@gmail.com>
2025-07-23 00:28:22 +0530

    util code disscussion

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 // database is always on another continent
 import dotenv from "dotenv";
 import connectDb from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({});
 
-connectDb();
+connectDb()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is Running at port:`, process.env.PORT);
+    });
+  })
+  .catch((err) => console.log("MONGO DB connection failed", err));
